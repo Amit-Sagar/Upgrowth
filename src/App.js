7d5d0e8e0b11a6c29dda8e892250a4eec3e9cf3c
Amit-Sagar <s.amitsagar99@gmail.com>
2024-12-15 16:06:07 +0530

    Configured tailwind and created folder structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl text-center font-bold underline">
+      <Header />
+      Hello world
+      <Footer />
     </div>
   );
 }
