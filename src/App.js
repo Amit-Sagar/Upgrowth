7ac6b032a2e329a20f5210e1f4ae8ecc0cb9d8fe
Amit-Sagar <s.amitsagar99@gmail.com>
2024-12-15 16:33:09 +0530

    dommy commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Footer from "./layout/Footer";
 import Header from "./layout/Header";
+import About from "./pages/About";
 
 function App() {
   return (
     <div className="text-3xl text-center font-bold underline">
       <Header />
-      Hello world
+      <About/>
       <Footer />
     </div>
   );
